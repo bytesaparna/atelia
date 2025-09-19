d52e4b4bcd09cf2ddf56b0f5a5502ff7ad50c180
Aparna Singh <aparnasingh1724@gmail.com>
2025-09-19 20:16:59 +0530

    Correct file name

--- config/app-config.ts
+++ config/app-config.ts
@@ -6,39 +6,39 @@ interface AppConfigItem {
 
 export const APP_CONFIG: AppConfigItem[] = [
     {
-        tokens_address: '/home/address/dress-one/tokens',
-        shares_address: '/home/address/dress-one/shares',
-        auction_address: '/home/address/dress-one/auction',
+        tokens_address: '/home/address/design-one/tokens',
+        shares_address: '/home/address/design-one/shares',
+        auction_address: '/home/address/design-one/auction',
     },
     {
-        tokens_address: '/home/address/dress-two/tokens',
-        shares_address: '/home/address/dress-two/shares',
-        auction_address: '/home/address/dress-two/auction',
+        tokens_address: '/home/address/design-two/tokens',
+        shares_address: '/home/address/design-two/shares',
+        auction_address: '/home/address/design-two/auction',
     },
     {
-        tokens_address: '/home/address/dress-three/tokens',
-        shares_address: '/home/address/dress-three/shares',
-        auction_address: '/home/address/dress-three/auction',
+        tokens_address: '/home/address/design-three/tokens',
+        shares_address: '/home/address/design-three/shares',
+        auction_address: '/home/address/design-three/auction',
     },
     {
-        tokens_address: '/home/address/dress-four/tokens',
-        shares_address: '/home/address/dress-four/shares',
-        auction_address: '/home/address/dress-four/auction',
+        tokens_address: '/home/address/design-four/tokens',
+        shares_address: '/home/address/design-four/shares',
+        auction_address: '/home/address/design-four/auction',
     },
     {
-        tokens_address: '/home/address/dress-five/tokens',
-        shares_address: '/home/address/dress-five/shares',
-        auction_address: '/home/address/dress-five/auction',
+        tokens_address: '/home/address/design-five/tokens',
+        shares_address: '/home/address/design-five/shares',
+        auction_address: '/home/address/design-five/auction',
     },
     {
-        tokens_address: '/home/address/dress-six/tokens',
-        shares_address: '/home/address/dress-six/shares',
-        auction_address: '/home/address/dress-six/auction',
+        tokens_address: '/home/address/design-six/tokens',
+        shares_address: '/home/address/design-six/shares',
+        auction_address: '/home/address/design-six/auction',
     },
     {
-        tokens_address: '/home/address/dress-seven/tokens',
-        shares_address: '/home/address/dress-seven/shares',
-        auction_address: '/home/address/dress-seven/auction',
+        tokens_address: '/home/address/design-seven/tokens',
+        shares_address: '/home/address/design-seven/shares',
+        auction_address: '/home/address/design-seven/auction',
     },
 ]
 
