065807b11404ca910ff3a209fe3b6e0546694d0b
Aparna Singh <aparnasingh1724@gmail.com>
2025-09-19 20:39:19 +0530

    Add contracts

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,8 @@
 namespace NodeJS {
     interface ProcessEnv {
         NEXT_PUBLIC_PROJECT_ID: string,
+        NEXT_PUBLIC_KERNEL_ADDRESS: string,
+        NEXT_PUBLIC_ADODB_ADDRESS: string,
+        NEXT_PUBLIC_VFS_ADDRESS: string,
     }
 }
