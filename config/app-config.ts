85541117c96a4dfdbf2d5f4e50602cd3de909026
Aparna Singh <aparnasingh1724@gmail.com>
2025-09-19 20:33:25 +0530

    Add address in app config

--- config/app-config.ts
+++ config/app-config.ts
@@ -6,39 +6,39 @@ interface AppConfigItem {
 
 export const APP_CONFIG: AppConfigItem[] = [
     {
-        tokens_address: '/home/address/design-one/tokens',
-        shares_address: '/home/address/design-one/shares',
-        auction_address: '/home/address/design-one/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-one/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-one/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-one/auction',
     },
     {
-        tokens_address: '/home/address/design-two/tokens',
-        shares_address: '/home/address/design-two/shares',
-        auction_address: '/home/address/design-two/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-two/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-two/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-two/auction',
     },
     {
-        tokens_address: '/home/address/design-three/tokens',
-        shares_address: '/home/address/design-three/shares',
-        auction_address: '/home/address/design-three/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-three/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-three/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-three/auction',
     },
     {
-        tokens_address: '/home/address/design-four/tokens',
-        shares_address: '/home/address/design-four/shares',
-        auction_address: '/home/address/design-four/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-four/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-four/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-four/auction',
     },
     {
-        tokens_address: '/home/address/design-five/tokens',
-        shares_address: '/home/address/design-five/shares',
-        auction_address: '/home/address/design-five/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-five/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-five/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-five/auction',
     },
     {
-        tokens_address: '/home/address/design-six/tokens',
-        shares_address: '/home/address/design-six/shares',
-        auction_address: '/home/address/design-six/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-six/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-six/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-six/auction',
     },
     {
-        tokens_address: '/home/address/design-seven/tokens',
-        shares_address: '/home/address/design-seven/shares',
-        auction_address: '/home/address/design-seven/auction',
+        tokens_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-seven/tokens',
+        shares_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-seven/shares',
+        auction_address: '/home/0xA6D02F2a4e6DBA17dc390F14e96398b4b491b249/design-seven/auction',
     },
 ]
 
